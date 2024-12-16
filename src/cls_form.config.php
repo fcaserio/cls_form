573da861b699e1b9cbe77cd582aa629f42fa5906
fcaserio <francisco.caserio@gmail.com>
2024-12-16 08:48:58 -0300

    src dir

---
+++ src/cls_form.config.php
@@ -0,0 +1,64 @@
+<?php
+// setup
+$this->css_path = "../css/admin.css";
+$this->css_print_path = "../css/print.css";
+$this->css_xls_path = "../css/admin_xls.css";
+$this->img_dir = "../images";
+$this->js_dir = "../js";
+$this->use_sysdoc = 0;
+$this->use_CalendarPopup = 1;
+$this->use_print_close = 0;
+$this->use_print_button = 0;
+$this->use_print_back = 0;
+$this->use_list_filter = 0;
+$this->use_required_position = "right";
+$this->setup_complete = "linked";
+$this->password_encrypt_method = "md5";
+// table
+$this->form_width = 980;
+$this->cellspacing = 3;
+$this->cellpadding = 5;
+$this->button_align = "right";
+// images
+$this->img_required = "<span class=\"req\">*</span>"; //"required.png";
+$this->img_bullet = "update.gif";
+$this->img_lock = "iconelock.gif";
+$this->img_unlock = "iconeunlock.gif";
+$this->img_print = "imprimir.gif";
+$this->img_xls = "icon_xls.gif";
+$this->img_anexo = "icon_anexo.gif";
+$this->img_calendar_popup = "icon_calendario.gif";
+$this->img_trash = "iconedelete.gif";
+// css
+$this->css_form = "form";
+$this->css_formpeq = "formpeq";
+$this->css_table = "table_form";
+$this->css_table_print = "table_form_print";
+$this->css_title_pg = "titulo";
+$this->css_confirm = "msg";
+$this->css_link_general = "link";
+$this->css_label = "tdtitulo";
+$this->css_text = "tdtexto";
+$this->css_label_print = "tdtitulo_print";
+$this->css_text_print = "tdtexto_print";
+$this->css_label_entity = "tdtitulob";
+$this->css_text_entity = "tdtextob";
+$this->css_required = "obrigatorio";
+$this->css_list_header = "header";
+$this->css_list_group = "tdtitulo";
+$this->css_list_title = "tdtitulo";
+$this->css_list_text = "tdtexto";
+//$this->css_list_even = "even_lista";
+$this->css_list_separator = "separator";
+$this->css_form_separator = "separator";
+$this->css_edit_list = "tdtexto";
+$this->css_page_by = "tdtexto";
+//$this->css_complete_insert_button_space = "insert_space";
+$this->css_button = "submit";
+$this->css_button_remove = "submit";
+$this->css_print_option = "print_option";
+$this->css_tab = "tdtitulo formtab onback";
+$this->css_tab_selected = "tdtitulo formtab";
+$this->css_debug = "debug";
+include "cls_form.lang.en.php";
+?>
---
+++ src/cls_form.lang.pt.php
@@ -0,0 +1,55 @@
+<?
+// titles
+$this->lang_title_insert = "CADASTRAR";
+$this->lang_title_update = "MODIFICAR";
+$this->lang_title_delete = "EXCLUIR";
+$this->lang_title_report = "RELATÓRIO DE";
+$this->lang_title_print = ""; // CONSULTAR
+$this->lang_title_report_stat = "RELATÓRIO ESTATÍSTICO";
+$this->lang_title_complete = "ADMINISTRAR";
+$this->lang_confirm_insert = "REGISTRO INSERIDO";
+$this->lang_confirm_update = "REGISTRO ATUALIZADO";
+$this->lang_confirm_delete = "REGISTRO(S) EXCLUIDO(S)";
+// labels
+$this->lang_label_required = "campos obrigatórios";
+$this->lang_label_no_select_NXN = "NÃO SELECIONADOS: clique para abrir";
+$this->lang_label_no_events_1XN = "Não há eventos para associação";
+$this->lang_label_non_active = "NÃO ATIVOS";
+// js
+$this->lang_js_field = "O campo";
+$this->lang_js_line = "na linha";
+$this->lang_js_req = "é de preenchimento obrigatório";
+$this->lang_js_format = "foi preenchido incorretamente";
+$this->lang_js_incomplete = "não foi completamente preenchido";
+$this->lang_js_file = "Arquivo inválido para o campo";
+$this->lang_js_file_auth = "formato esperado";
+$this->lang_js_password = "deve ser igual à confirmação";
+$this->lang_js_ent = "Obrigatório informar";
+$this->lang_js_unique = "Já existe um registro no banco de dados para";
+$this->lang_js_min = "deve ser maior ou igual a";
+$this->lang_js_max = "deve ser menor ou igual a";
+// form
+$this->lang_button_label = "ok";
+$this->lang_close_button_label = "Fechar";
+$this->lang_print_button_label = "Imprimir";
+$this->lang_back_button_label = "Voltar";
+$this->lang_search_button_label = "Buscar";
+$this->lang_form_dropdown_choose = "Escolha";
+$this->lang_form_no_defined = "NÃO REGISTRADO";
+$this->lang_not_found = "Nenhum resultado encontrado";
+$this->lang_return_pk = "CÓDIGO GERADO";
+$this->lang_remove_image = "Remover Imagem";
+$this->lang_remove_file = "Remover Arquivo";
+$this->lang_filter = "Filtrar em";
+$this->lang_password_confirm = "Confirmar";
+$this->lang_password_alter = "Alterar";
+$this->lang_prompt_entity = "Quantas entradas deseja adicionar?";
+$this->lang_global_edition = "Edição Global";
+$this->lang_delete_dependency = "Não é permitida exclusão de registros relacionados à tabela";
+$this->lang_new_entity_complete = "Clique abaixo para complementar o cadastro dos novos registros criados";
+$this->lang_print_label = "VERSÃO PARA IMPRESSÃO";
+$this->lang_file_format = "FORMATO DE IMAGEM INVÁLIDO PARA O ARQUIVO";
+$this->lang_file_format_types = "FORMATOS ACEITOS: JPG, PNG ou GIF";
+$this->lang_wait = "Aguarde...";
+$this->lang_other = "OUTRO";
+?>
